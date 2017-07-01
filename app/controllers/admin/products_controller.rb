e4e1364ddcb1e1f11751384e8a0c293fcba1666a
OS X <lyfirst@naver.com>
2017-07-01 21:47:14 +0900

    admin product CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,63 @@
+class Admin::ProductsController < ApplicationController
+
+
+  def index
+    @products = Product.all
+  end
+
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
+  def new
+    @product = Product.new
+  end
+
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path
+      flash[:notice] = "Updated"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:alert] = "Deleted"
+    redirect_to admin_products_path
+  end
+
+
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price)
+  end
+
+
+end
