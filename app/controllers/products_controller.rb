3582f8b47f0d3baab7b79e0a14a0c92c52af55f9
OS X <lyfirst@naver.com>
2017-07-01 23:15:24 +0900

    product show

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,33 @@
+class ProductsController < ApplicationController
+
+
+  def index
+    @products = Product.all
+  end
+
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
