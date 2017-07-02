802f019ae25615bb8318be1db5479f61149ca77d
OS X <lyfirst@naver.com>
2017-07-02 13:10:18 +0900

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,7 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
 
-
+  has_many :product_lists
 
 
 
---
+++ app/models/product_list.rb
@@ -0,0 +1,14 @@
+class ProductList < ApplicationRecord
+
+
+  belongs_to :order
+
+
+
+
+
+
+
+
+
+end
