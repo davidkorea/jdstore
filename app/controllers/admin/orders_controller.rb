b95051b48f18e6a57b1ef6bbbbf98a8778bba69c
OS X <lyfirst@naver.com>
2017-07-02 14:17:47 +0900

    add ship/shipped/cancel/return action to admin/orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -17,6 +17,48 @@ class Admin::OrdersController < ApplicationController
  end
 
 
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to :back
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_order!
+    redirect_to :back
+  end
+
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_to :back
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  def admin_required
    if !current_user.admin?
